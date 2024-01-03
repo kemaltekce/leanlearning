30b9bb3e24c6197802fee2a2b376df1d8cac1aef
kemal <kemal.tekce@gmail.com>
2024-01-03 20:47:10 +0100

    adjust styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,20 @@ module.exports = {
     extend: {
       animation: {
         scroll: 'scroll 1.5s linear infinite',
+        banner: 'banner 25s linear infinite',
       },
       keyframes: {
         scroll: {
           '0%': { transform: 'translateY(-100%)' },
           '100%': { transform: 'translateY(100%)' },
+        },
+        banner: {
+          '0%': {
+            transform: 'translateX(100vw)',
+          },
+          '100%': {
+            transform: 'translateX(-100%)',
+          },
         },
       },
     },
